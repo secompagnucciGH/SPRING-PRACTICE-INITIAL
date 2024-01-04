a52671e5220d4dd412023ad53bb9ab9aa3b07f68
sebastian <j4guarsj4@gmail.com>
2024-01-04 10:55:07 -0300

    adición de comentarios descriptivos .XML UsoEmpleados.java

--- src/IoC/UsoEmpleados.java
+++ src/IoC/UsoEmpleados.java
@@ -5,44 +5,41 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UsoEmpleados {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-//        Empleados Empleado1 = new DirectorEmpleado();
-//
-//        System.out.println(Empleado1.getTareas());
-
+        // Crear un contexto de aplicación utilizando el archivo XML de configuración
         ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("file:src/aplicationContext.xml");
 
-        // EMPLEADO PARA EL CONSTRUCTOR CON DEPENDENCIA jefe y director.
-        // llama al bean miEmpleado, usa la clase Empleados
+        // Crear un objeto 'Juan' que representa a un empleado que es un JefeEmpleado
+        // Obtiene el bean 'miEmpleado' del contexto y usa la clase Empleados
         Empleados Juan = contexto.getBean("miEmpleado", Empleados.class);
 
-        // muestra las tareas de el director
+        // Mostrar las tareas del empleado (en este caso, el director)
         System.out.println(Juan.getTareas());
 
-        // muestra informes
+        // Mostrar informes generados por el empleado
         System.out.println(Juan.getInforme());
         System.out.println("-----------------------------------XXXXX------------------------------------------");
 
-
-        // EMPLEADO PARA EL SETTER, SECRETARIO
-        // se modifica el bean, ahora no es miEmpleado, ahora es miEmpleadoSecretario
+        // Crear un objeto 'Maria' que representa a un empleado que es un SecretarioEmpleado
+        // Modificar el bean de Spring para usar el SecretarioEmpleado y obtenerlo del contexto
         SecretarioEmpleado Maria = contexto.getBean("miSecretarioEmpleado", SecretarioEmpleado.class);
 
-        // muestra las tareas de el director
+        // Mostrar las tareas del empleado (en este caso, el secretario)
         System.out.println(Maria.getTareas());
 
-        // muestra informes
-
+        // Mostrar informes generados por el empleado
         System.out.println("INFORME: ------" + Maria.getInforme());
 
+        // Mostrar el nombre de la empresa del empleado secretario
         System.out.println("NOMBRE EMPRESA: ------" + Maria.getNombreEmpresa());
 
+        // Mostrar el email del empleado secretario
         System.out.println("EMAIL: ------" + Maria.getEmail());
 
         System.out.println("-----------------------------------XXXXX------------------------------------------");
 
-
+        // Cerrar el contexto de la aplicación para liberar recursos
         contexto.close();
     }
 }
